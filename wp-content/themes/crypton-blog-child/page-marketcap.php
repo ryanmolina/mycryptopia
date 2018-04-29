8329c21afb31a3f0aa92d9bafb5cd71f0b08381c
ryanmolina <ryan.mo@ibinex.tech>
2018-04-29 16:35:12 +0800

    market cap mobile sorting

--- wp-content/themes/crypton-blog-child/page-marketcap.php
+++ wp-content/themes/crypton-blog-child/page-marketcap.php
@@ -1,15 +1,23 @@
 <?php
 get_header();
 ?>
-    <span>Sort by:</span>
-    <select id="sort-by">
-		<option value="rank">Rank</option>
-		<option value="name">Name</option>
-		<option value="price">Price</option>
-		<option value="cap">Market Cap</option>
-		<option value="volume">Volume 24H</option>
-		<option value="change">Change 24H</option>
-    </select>
+	<div class="marketcap-sorting">
+		<div class="marketcap-sort-category">
+			<select id="sort-category">
+				<option value="rank">Rank</option>
+				<option value="name">Name</option>
+				<option value="price">Price</option>
+				<option value="cap">Market Cap</option>
+				<option value="volume">Volume 24H</option>
+				<option value="change">Change 24H</option>
+			</select>
+		</div>
+
+		<div id="sort-by">
+			<i class="icon-arrow-up"></i>
+			<i class="icon-arrow-down"></i>
+		</div>
+	</div>
     <table class="marketcap-table" id="marketcap">
       	<thead class="marketcap-table-head">
         	<tr class="marketcap-table-row">
