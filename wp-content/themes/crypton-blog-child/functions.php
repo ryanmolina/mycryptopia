f323eec072f96d0402fb6e6d17d9eb164cd0043c
ryanmolina <ryan.mo@ibinex.tech>
2018-04-30 16:37:35 +0800

    Add parent theme style.css as dependency to child theme style.css

--- wp-content/themes/crypton-blog-child/functions.php
+++ wp-content/themes/crypton-blog-child/functions.php
@@ -4,7 +4,13 @@
  */
 
 function crypton_blog_child_scripts() {
-    wp_enqueue_style('crypton_blog-parent-style', get_template_directory_uri(). '/style.css' );
+	$parent_style = 'crypton_blog-parent-style';
+    wp_enqueue_style($parent_style, get_template_directory_uri(). '/style.css' );
+    wp_enqueue_style('crypton_blog-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
     wp_enqueue_script('helper-script', get_stylesheet_directory_uri(). '/js/helper.js', null, null, true);
     wp_enqueue_script('ico-calendar-script', get_stylesheet_directory_uri(). '/js/ico-calendar.js', array('jquery'), null, true); 
     wp_enqueue_script('marketcap-script', get_stylesheet_directory_uri(). '/js/marketcap.js', array('jquery'), null, true);
