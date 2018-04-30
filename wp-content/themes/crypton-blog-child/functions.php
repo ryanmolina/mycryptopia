83a65112730a43faabe682773ae5dab0ea64f062
ryanmolina <ryan.mo@ibinex.tech>
2018-04-30 18:00:02 +0800

    Delete javascript defer parsing in functions.php

--- wp-content/themes/crypton-blog-child/functions.php
+++ wp-content/themes/crypton-blog-child/functions.php
@@ -18,11 +18,4 @@ function crypton_blog_child_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'crypton_blog_child_scripts' );
 
-
-function defer_parsing_of_js ( $url ) {
-	if ( FALSE === strpos( $url, '.js' ) ) return $url;
-	if ( strpos( $url, 'jquery.js' ) ) return $url;
-	return "$url' defer ";
-}
-add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 ); 
 ?>
